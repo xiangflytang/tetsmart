5358610ad6c0bbf3456b8f94333ce86990e5ed34
xiangflytang <tangxiang023@gmail.com>
2015-04-13 16:46:26 -0400

    shopInfo WSDL customizing WSDL

--- src/com/kaushik/javabrains/service/ProductCatalog.java
+++ src/com/kaushik/javabrains/service/ProductCatalog.java
@@ -8,20 +8,24 @@ import javax.jws.WebService;
 
 import com.kaushik.javabrains.business.ProductServiceImpl;
 
-@WebService
+@WebService(name="TestMartCatalog",portName="TestMartCatalogPort"
+				,serviceName="TestMartCatalogService"
+				,targetNamespace="http://www.testmart.com")
 public class ProductCatalog {
 	ProductServiceImpl productService=new ProductServiceImpl();
 	
-	@WebMethod
+	@WebMethod(action="fetch_Categories", operationName="fetchCategories")
 	public List<String>getProductCategories(){
 		return productService.getProductCategories();
 	
 	}
 	
+	@WebMethod(exclude=true)
 	public List<String> getProducts(String category){
 		return productService.getProducts(category);
 	}
 	
+	@WebMethod(exclude=true)
 	public boolean addProduct(String category,String product){
 		return productService.addProduct(category, product);
 	}
